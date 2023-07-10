c6d11a8cf3ddac149c0545559603a9b110087f19
Manny <mannythefay@gmail.com>
2023-07-10 17:05:41 +0200

    Add .env and blade support

--- nvim/lua/user/core/plugins.lua
+++ nvim/lua/user/core/plugins.lua
@@ -521,12 +521,28 @@ use({
 --[[ }) ]]
 
 -- Command line auto complete style:
-use {
+use({
   'gelguy/wilder.nvim',
   config = function()
     require('user.plugin_options.wilder')
   end,
-}
+})
+
+
+-- .env file support:
+use({
+  'ellisonleao/dotenv.nvim',
+  config = function()
+    require('user.plugin_options.dotenv')
+  end,
+})
+
+
+-- Blade syntax highligting:
+use({
+  'jwalton512/vim-blade'
+})
+
 
 if packer_bootstrap then
   packer.sync()

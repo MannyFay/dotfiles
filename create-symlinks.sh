0f16be6e6c0af25a76a9b908eb64d205ca28703a
Manny <mannythefay@gmail.com>
2023-07-18 00:01:51 +0200

    Add yabai to setup

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -3,6 +3,57 @@
 DOTFILES="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 
+#-----------------------------------------------------------
+#- Git
+
+# Delete .gitconfig if there is one in ~/:
+rm -rf $HOME/.gitconfig
+
+# Create symlink to ~/.dotfiles/git/.gitconfig:
+ln -s $DOTFILES/git/.gitconfig $HOME/.gitconfig
+
+# Delete ~/.gitignore_global:
+rm -rf $HOME/.gitignore_global
+
+# Create symlink to ~/.dotfiles/git/.gitignore_global:
+ln -s $DOTFILES/git/.gitignore_global $HOME/.gitignore_global
+
+
+#-----------------------------------------------------------
+#- Neovim
+
+# Delete nvim directory if there is one in ~/.config:
+rm -rf $HOME/.config/nvim
+# Create symlink to nvim directory in /.dotfiles:
+ln -s $DOTFILES/nvim $HOME/.config/nvim
+
+
+#-----------------------------------------------------------
+#- PhpStorm
+
+# Delete ~/.ideavimrc:
+rm -rf $HOME/.ideavimrc
+
+# Create symlink to ~/.dotfiles/phpstorm/.ideavimrc:
+ln -s $DOTFILES/phpstorm/.ideavimrc $HOME/.ideavimrc
+
+
+#-----------------------------------------------------------
+#- Yabai
+
+# Delete ~/.yabairc
+rm -rf $HOME/.yabairc
+# Create symlink to ~/.dotfiles/yabai/.yabairc:
+ln -s $DOTFILES/yabai/.yabairc $HOME/.yabairc
+
+# Delete ~/.skhdrc:
+rm -rf $HOME/.skhdrc
+# Create symlink to ~/.dotfiles/yabai/.skhdrc:
+ln -s $DOTFILES/yabai/.skhdrc $HOME/.skhdrc
+
+yabai --restart-service
+skhdrc --restart-service
+
 
 #-----------------------------------------------------------
 #- ZSH
@@ -21,56 +72,5 @@ ln -s $DOTFILES/zsh/.zsh_aliases $HOME/.zsh_aliases
 rm -rf $HOME/.oh-my-zsh/themes/mannydark.zsh-theme
 # Create symlink to mannydark theme in /.dotfiles/zsh:
 ln -s $DOTFILES/zsh/mannydark.zsh-theme $HOME/.oh-my-zsh/themes/mannydark.zsh-theme
-
-
-
-#rm -rf ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-#mkdir ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/
-#ln -s /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-#ln -s /opt/homebrew/Cellar/zsh-autosuggestions/zsh-autosuggestions.zsh ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-
-#-----------------------------------------------------------
-#- Neovim
-
-# Delete nvim directory if there is one in ~/.config:
-rm -rf $HOME/.config/nvim
-# Create symlink to nvim directory in /.dotfiles:
-ln -s $DOTFILES/nvim $HOME/.config/nvim
-
-
-#-----------------------------------------------------------
-#- Kitty
-
-# Delete ~/.config/kitty directory:
-rm -rf $HOME/.config/kitty
-
-# Create symlink to ~/.dotfiles/kitty:
-ln -s $DOTFILES/kitty $HOME/.config/kitty
-
-
-#-----------------------------------------------------------
-#- Git
-
-# Delete .gitconfig if there is one in ~/:
-rm -rf $HOME/.gitconfig
-
-# Create symlink to ~/.dotfiles/git/.gitconfig:
-ln -s $DOTFILES/git/.gitconfig $HOME/.gitconfig
-
-# Delete ~/.gitignore_global:
-rm -rf $HOME/.gitignore_global
-
-# Create symlink to ~/.dotfiles/git/.gitignore_global:
-ln -s $DOTFILES/git/.gitignore_global $HOME/.gitignore_global
-
-
-#-----------------------------------------------------------
-#- PhpStorm
-
-# Delete ~/.ideavimrc:
-rm -rf $HOME/.ideavimrc
-
-# Create symlink to ~/.dotfiles/phpstorm/.ideavimrc:
-ln -s $DOTFILES/phpstorm/.ideavimrc $HOME/.ideavimrc
 
 
---
+++ yabai/shortcuts.md
@@ -0,0 +1,12 @@
+# Yabai Shortcuts
+
+| Shortcut    | Function                             |
+|:-----------:|:-------------------------------------|
+| `alt` + `j` | To the window below                  |
+| `alt` + `k` | To the window above                  |
+| `alt` + `h` | To the left window                   |
+| `alt` + `l` | To the right window                  |
+| `cmd` + `n` | Open new terminal (macOS shortcut)   |
+| `cmd` + `w` | Close active window (macOS shortcut) |
+| `alt` + `s` | External display on the left         |
+| `alt` + `f` | External display on the right        |

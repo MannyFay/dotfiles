faa7f1d7990c7fbb0b4ddb276a6dce84097d9771
Manny Fay <mannythefay@gmail.com>
2024-01-21 21:38:13 +0100

    Update spelling library

--- nvim/lua/user/core/options.lua
+++ nvim/lua/user/core/options.lua
@@ -44,5 +44,4 @@ vim.opt.equalalways           = false                        -- Disable automati
 vim.cmd('set pumblend=0')                                    -- Transparency of floating windows.
 vim.loader.enable()                                          -- Improve Neovim startup time.
 vim.opt.mouse = 'a'
-
 -- nvim_list_uis()[0]

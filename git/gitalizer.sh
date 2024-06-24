098027c2fc8bdd2581c5c9d4deee486a0740eaf3
Manny Fay <mannythefay@gmail.com>
2024-06-25 01:02:55 +0200

    Reduce search only on git directories on disk.

--- git/gitalizer.sh
+++ git/gitalizer.sh
@@ -5,15 +5,23 @@ check_git_repo() {
     local repo_dir=$1
     cd "$repo_dir"
     if ! git status | grep -q 'nothing to commit, working tree clean'; then
-        echo "Uncommittete Änderungen in $repo_dir:"
+        printf "\033[31m----- Uncommitted changes in: ----------------------------------------\033[0m\n\033[34m%s\033[0m\n" "$repo_dir"
         git status -s
     fi
 }
 
 export -f check_git_repo
 
-# Search for all .git directories and check all of them:
-find / -type d -name .git 2>/dev/null | while read -r git_dir; do
-    repo_dir=$(dirname "$git_dir")
-    check_git_repo "$repo_dir"
+search_directories=(
+    $HOME/personal/*
+    $HOME/work/*
+  )
+
+# Loop through specified directories and check for Git repositories:
+for dir in "${search_directories[@]}"; do
+    # Find all .git directories within the current directory and subdirectories
+    find "$dir" -type d -name .git 2>/dev/null | while read -r git_dir; do
+        repo_dir=$(dirname "$git_dir")
+        check_git_repo "$repo_dir"
+    done
 done

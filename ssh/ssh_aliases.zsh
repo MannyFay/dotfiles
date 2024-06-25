220ea58b200c2084f7f6cf5edd2648f96363ffeb
Manny Fay <mannythefay@gmail.com>
2024-06-25 21:30:40 +0200

    Update SSH-Aliases.

--- ssh/ssh_aliases.zsh
+++ ssh/ssh_aliases.zsh
@@ -1,8 +1,7 @@
 
 #------------------------------------------------------------------------------
 #----- SSH Aliases
+#------------------------------------------------------------------------------
 
 alias sshe='eval "$(ssh-agent -s)"'                                      # Start SSH-Agent and set its environment variables.
-alias sshpgh='eval "$(ssh-agent -s)" && ssh-add ~/.ssh/personal-github'  # Activate personal GitHub SSH.
-alias sshpgl='eval "$(ssh-agent -s)" && ssh-add ~/.ssh/personal-gitlab'  # Activate personal GitLab SSH.
-alias sshwgh='eval "$(ssh-agent -s)" && ssh-add ~/.ssh/vds_ubuntu'       # Activate work GitHub SSH.
+alias sshpk='eval "$(ssh-agent -s)" && ssh-add ~/.ssh/personal-ssh-key'  # Activate personal SSH key.

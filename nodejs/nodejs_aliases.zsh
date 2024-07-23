31f722e34cb5dbca1f0ccdea0470bb7510d05d45
Manny Fay <mannythefay@gmail.com>
2024-07-23 11:16:31 +0200

    WIP

---
+++ nodejs/nodejs_aliases.zsh
@@ -0,0 +1,13 @@
+#------------------------------------------------------------------------------
+#  Node.js ZSH Aliases
+#------------------------------------------------------------------------------
+
+# This file has to be sourced in .zsh_aliases file.
+# Copy this line to do so:
+# source ~/personal/github/dotfiles/nodejs/nodejs_aliases.zsh                   # Node.js
+
+#------------------------------------------------------------------------------
+#----- Yarn
+
+alias yip='yarn'   # Install packages.
+alias yd='yarn dev' # Run development server.

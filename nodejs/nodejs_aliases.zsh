50f06c7a9c0deb4327441e5997bcfc9726650ee0
Manny Fay <mannythefay@gmail.com>
2024-09-24 23:33:35 +0200

    WIP

--- nodejs/nodejs_aliases.zsh
+++ nodejs/nodejs_aliases.zsh
@@ -6,6 +6,15 @@
 # Copy this line to do so:
 # source ~/personal/github/dotfiles/nodejs/nodejs_aliases.zsh                   # Node.js
 
+
+#------------------------------------------------------------------------------
+#----- npm
+
+alias nrd='npm run dev'             # Run development server.
+alias ni='npm install'              # Install packages.
+alias nid='npm install --save-dev'  # Install packages as devDependencies.
+
+
 #------------------------------------------------------------------------------
 #----- Yarn
 
---
+++ orm/prisma/prisma_aliases.zsh
@@ -0,0 +1,13 @@
+#------------------------------------------------------------------------------
+#  Prisma ORM ZSH Aliases
+#------------------------------------------------------------------------------
+
+# This file has to be sourced in .zsh_aliases file.
+# Copy this line to do so:
+# source ~/personal/github/dotfiles/orm/prisma/prisma_aliases.zsh       # Prisma ORM
+
+
+#------------------------------------------------------------------------------
+#----- Prisma CLI
+
+alias prisgen='npx prisma generate'  # Generate Prisma Client.

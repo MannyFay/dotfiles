c920ef911657dd9470999733e6921985e59af966
Manny Fay <mannythefay@gmail.com>
2024-10-29 11:52:35 +0100

    Update Mac and tmux environment.

--- nodejs/nodejs_aliases.zsh
+++ nodejs/nodejs_aliases.zsh
@@ -6,17 +6,19 @@
 # Copy this line to do so:
 # source ~/personal/github/dotfiles/nodejs/nodejs_aliases.zsh                   # Node.js
 
-
 #------------------------------------------------------------------------------
 #----- npm
 
-alias nrd='npm run dev'             # Run development server.
-alias ni='npm install'              # Install packages.
-alias nid='npm install --save-dev'  # Install packages as devDependencies.
-
+alias nrd='npm run dev'            # Run development server.
+alias ni='npm install'             # Install packages.
+alias nid='npm install --save-dev' # Install packages as devDependencies.
 
 #------------------------------------------------------------------------------
 #----- Yarn
 
-alias yip='yarn'     # Install packages.
-alias yd='yarn dev'  # Run development server.
+alias yip='yarn install'         # Install packages.
+alias yd='yarn dev'              # Run development server.
+alias yla='yarn eslint .'        # Lint the code in the current directory.
+alias ylfa='yarn eslint --fix .' # Lint the code in the current directory and fix the issues.
+alias yl='yarn eslint'           # Lint the code of a file by adding its name.
+alias ylff='yarn eslint --fix'   # Lint the code of a file by adding its name and fix the issues.
--- tmux/tmux-sessionizer.sh
+++ tmux/tmux-sessionizer.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
 if [[ $# -eq 1 ]]; then
-    selected=$1
+  selected=$1
 else
-    selected=$(find ~ ~/personal/github ~/personal/gitlab ~/work/fitcrew ~/work/fitcrew/gitlab ~/work/vds ~/work/vds/github /Volumes/Users/manny/Library/Mobile\ Documents/iCloud~md~obsidian/Documents -mindepth 1 -maxdepth 1 -type d | fzf)
+  selected=$(find ~ ~/personal/github ~/personal/github/contributions ~/personal/gitlab ~/work/fitcrew ~/work/fitcrew/gitlab ~/work/vds ~/work/vds/github /Volumes/Users/manny/Library/Mobile\ Documents/iCloud~md~obsidian/Documents -mindepth 1 -maxdepth 1 -type d | fzf)
+  # selected=$(fd . ~ ~/personal/github ~/personal/github/contributions ~/personal/gitlab ~/work/fitcrew ~/work/fitcrew/gitlab ~/work/vds ~/work/vds/github /Volumes/Users/manny/Library/Mobile\ Documents/iCloud~md~obsidian/Documents --min-depth 1 --max-depth 1 --type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
-    exit 0
+  exit 0
 fi
 
 if [[ ! -d $selected ]]; then
-    echo "Directory '$selected' does not exist."
-    exit 1
+  echo "Directory '$selected' does not exist."
+  exit 1
 fi
 
 selected_name=$(basename "$selected" | tr . _)
@@ -20,15 +21,15 @@ tmux_running=$(pgrep tmux)
 
 # Check if tmux is not running or if $TMUX is not set
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-    # Start a new tmux session with the selected directory
-    tmux new-session -s "$selected_name" -c "$selected"
-    exit 0
+  # Start a new tmux session with the selected directory
+  tmux new-session -s "$selected_name" -c "$selected"
+  exit 0
 fi
 
 # Check if the session exists
 if ! tmux has-session -t="$selected_name" 2>/dev/null; then
-    # If session doesn't exist, start a detached tmux session
-    tmux new-session -ds "$selected_name" -c "$selected"
+  # If session doesn't exist, start a detached tmux session
+  tmux new-session -ds "$selected_name" -c "$selected"
 fi
 
 # Switch to the tmux session

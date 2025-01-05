c046f58e0a92c5d8cc22ff193dafb9d1864717f4
Manny Fay <mannythefay@gmail.com>
2025-01-05 10:23:46 +0100

    finish eza setup.

--- cli-tools/eza/eza_aliases.zsh
+++ cli-tools/eza/eza_aliases.zsh
@@ -4,10 +4,6 @@
 
 # This file has to be sourced in .zshrc file.
 
-# alias l='eza -alHiUm'                                                                # list all files in current directory.
-# alias l='eza --all --long --links --inode --created --modified'
-
-
 # Command results ordered by type (directories first):
 alias l='eza --color=always --icons=always --header --group-directories-first --sort=type --sort=name --all --long --links --inode --group --created --modified --git --git-repos --time-style="+%a, %Y-%m-%d @ %H:%M "'
 
@@ -16,4 +12,3 @@ alias ls='eza --color=always --icons=always --header --group-directories-first -
 
 # Command results listed in columns:
 alias lc='eza --color=always --icons=always --header --grid --all --long --links --inode --created --modified --git --git-repos --time-style="+%Y/%m/%d %H:%M"'
-
